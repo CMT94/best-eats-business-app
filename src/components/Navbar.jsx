2bff23ae4e375a136bbfc18c4211d730afed8041
Clément M <cmt94.dev@gmail.com>
2023-09-13 20:31:05 +0200

    static sidedrawer v1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
-import { BsFillCartFill } from "react-icons/bs";
+import {
+  AiOutlineClose,
+  AiOutlineMenu,
+  AiOutlineSearch,
+  AiFillTag,
+} from "react-icons/ai";
+import { BsFillCartFill, BsFillSaveFill } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
+import { MdFavorite, MdHelp } from "react-icons/md";
+import { FaUserFriends, FaWallet } from "react-icons/fa";
+
+const NAV_LINKS = [
+  { title: "Orders", icon: <TbTruckDelivery size={25} className="mr-4" /> },
+  { title: "Favorites", icon: <MdFavorite size={25} className="mr-4" /> },
+  { title: "Wallet", icon: <FaWallet size={25} className="mr-4" /> },
+  { title: "Promotions", icon: <AiFillTag size={25} className="mr-4" /> },
+  { title: "Help", icon: <MdHelp size={25} className="mr-4" /> },
+  { title: "Best One", icon: <BsFillSaveFill size={25} className="mr-4" /> },
+  {
+    title: "Invite Friends",
+    icon: <FaUserFriends size={25} className="mr-4" />,
+  },
+];
 
 const Navbar = () => {
   const [isDeliverySelected, setIsDeliverySelected] = React.useState(true);
@@ -49,11 +70,38 @@ const Navbar = () => {
           className="bg-transparent p-2 w-full focus:outline-none"
         />
       </div>
-      <div>
-        <button className="bg-black text-white rounded-full hover:bg-gray-800 transition duration-200 ease-in-out hidden md:flex items-center py-2">
-          <BsFillCartFill size={20} className="mr-2" />
-          <span>Cart</span>
-        </button>
+
+      <button className="bg-black text-white rounded-full hover:bg-gray-800 transition duration-200 ease-in-out hidden md:flex items-center py-2">
+        <BsFillCartFill size={20} className="mr-2" />
+        <span>Cart</span>
+      </button>
+
+      {/* Overlay */}
+      <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+
+      {/* Sidedrawer */}
+      <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
+        <AiOutlineClose
+          size={30}
+          className="absolute right-4 top-4 cursor-pointer"
+        />
+        <h2 className="text-2xl p-4">
+          Best <span className="font-bold">Eats</span>
+        </h2>
+
+        <nav>
+          <ul className="flex flex-col p-4 text-gray-800">
+            {NAV_LINKS.map((link, linkIndex) => (
+              <li
+                key={linkIndex}
+                className="text-xl py-4 flex items-center cursor-pointer"
+              >
+                <span>{link.icon}</span>
+                <span>{link.title}</span>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </div>
   );
