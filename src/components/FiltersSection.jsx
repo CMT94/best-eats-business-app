0184250c872f0b2efb2faf8cfc9a53616b1fc07f
Clément M <cmt94.dev@gmail.com>
2023-09-14 14:44:42 +0200

    dynamic filters rendering + filters handler

--- src/components/FiltersSection.jsx
+++ src/components/FiltersSection.jsx
@@ -1,44 +1,47 @@
 import React from "react";
 
-const FiltersSection = () => {
+const FILTERS_TYPE = [
+  { title: "All", category: undefined },
+  { title: "Burgers", category: "burger" },
+  { title: "Pizza", category: "pizza" },
+  { title: "Salads", category: "salad" },
+  { title: "Chicken", category: "chicken" },
+];
+
+const FILTERS_PRICE = ["$", "$$", "$$$", "$$$$"];
+
+const FiltersSection = (props) => {
+  const { onFilterType, onFilterPrice } = props;
+
   return (
     <div className="flex flex-col lg:flex-row justify-between">
       <div>
         <p className="font-bold text-gray-700">Filter Type</p>
         <div className="flex justify-between flex-wrap">
-          <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200">
-            All
-          </button>
-          <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200">
-            Burgers
-          </button>
-          <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200">
-            Pizza
-          </button>
-          <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200">
-            Salads
-          </button>
-          <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200">
-            Chicken
-          </button>
+          {FILTERS_TYPE.map((filter, filterIndex) => (
+            <button
+              key={filterIndex}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200"
+              onClick={() => onFilterType(filter.category)}
+            >
+              {filter.title}
+            </button>
+          ))}
         </div>
       </div>
 
       <div>
         <p className="font-bold text-gray-700">Filter Price</p>
         <div className="flex justify-between max-w-[390px] w-full">
-          <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200">
-            $
-          </button>
-          <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200">
-            $$
-          </button>
-          <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200">
-            $$$
-          </button>
-          <button className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200">
-            $$$$
-          </button>
+          {FILTERS_PRICE.map((filter, filterIndex) => (
+            <button
+              key={filterIndex}
+              className="m-1 border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white duration-200"
+              onClick={() => onFilterPrice(filter)}
+            >
+              {filter}
+            </button>
+          ))}
         </div>
       </div>
     </div>
--- src/components/FoodCardsList.jsx
+++ src/components/FoodCardsList.jsx
@@ -8,13 +8,31 @@ import Card from "./shared/Card";
 const FoodCardsList = () => {
   const [foodsList, setFoodsList] = React.useState(foodsData);
 
+  const filterTypeHandler = (category) => {
+    let tmpFilteredList = [...foodsData];
+
+    if (category) {
+      tmpFilteredList = foodsData.filter((item) => item.category === category);
+    }
+
+    setFoodsList(tmpFilteredList);
+  };
+
+  const filterPriceHandler = (price) => {
+    const tmpFilteredList = foodsData.filter((item) => item.price === price);
+    setFoodsList(tmpFilteredList);
+  };
+
   return (
     <div className="max-w-[1640px] m-auto px-4 py-12">
       <h1 className="text-orange-600 font-bold text-4xl text-center">
         Top Rated Menu Items
       </h1>
 
-      <FiltersSection />
+      <FiltersSection
+        onFilterType={filterTypeHandler}
+        onFilterPrice={filterPriceHandler}
+      />
 
       <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
         {foodsList.map((foodItem) => (
